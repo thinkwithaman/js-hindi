95a2b91d094dbcbeec6534d0e58fc82b544c5193
Aman <amanmyself3@gmail.com>
2026-01-21 14:12:59 +0000

    lesson 1 variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+/*variables in js
+there are three types of variables.
+
+let 
+const
+var
+*/
+let userName = "Rishu"
+const phoneNumber = 8810576455
+var userCity = "Delhi"
+
+/* var variable is not validate now in the coding sector coz of the problem it has
+
+1.) it doesn't resepect {}[].
+*/
